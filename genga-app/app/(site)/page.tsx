4378e664ca23701c9fe5255db3088ff3add96522
Tushar Dubey <tdubey@terpmail.umd.edu>
2024-07-07 19:39:53 -0400

    init

---
+++ genga-app/app/(site)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-blue-500">
+      Hello Genga!
+    </div>
+  );
+}
---
+++ genga-app/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Genga",
+  description: "Guess the anime based on the given clues!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}</body>
+    </html>
+  );
+}
