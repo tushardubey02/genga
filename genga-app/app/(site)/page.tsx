3a26e52f76fd3b3d41c478fc3e744361758f43dd
Tushar Dubey <tdubey@terpmail.umd.edu>
2024-07-09 00:09:16 -0400

    started on layout

--- genga-app/app/(site)/page.tsx
+++ genga-app/app/(site)/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-blue-500">
-      Main content
+    <div>
+      <div className=" flex text-gray-400 text-5xl font-semibold content-center justify-center m-3">
+          Genga
+      </div>
+      <div className="flex justify-center text-gray-400 text-xl font-medium">
+            Guess the anime based on the given clues!
+      </div>
     </div>
+    
   );
 }
