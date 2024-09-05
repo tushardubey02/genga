bc6e8fbad13bf59dc5938f07c5397f88bdb33e35
Tushar Dubey <tdubey@terpmail.umd.edu>
2024-09-05 17:13:02 -0400

    works with sample data

--- genga-app/server/server.py
+++ genga-app/server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Define a route that returns JSON data
 @app.route('/get_data', methods=['GET'])
