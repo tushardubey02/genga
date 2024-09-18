09fd28a69da28de10591abaa87fa8d5c887ce215
Tushar Dubey <tdubey@terpmail.umd.edu>
2024-09-18 00:04:31 -0400

    added more anime

--- genga-app/server/data.py
+++ genga-app/server/data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_show() -> dict:
     df = pd.read_csv('/Users/tushardubey/Personal Projects/genga/genga-app/data/anime.csv')
-    random_index = np.random.randint(1, 201)  # Random number between 1 and 200
+    random_index = np.random.randint(1, 500)  # Random number between 1 and 200
     selection = df.iloc[random_index]  # Select the row at the random index
     print(selection)   # Print the selected row
     return selection.to_dict()  # Convert the row to a dictionary
