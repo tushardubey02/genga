25d80f84bd49d046b3ffcc03dd3224f7b9020799
Tushar Dubey <tushar.dubey@gmail.com>
2024-10-16 00:34:32 -0400

    avoid unknown titles

--- genga-app/server/data.py
+++ genga-app/server/data.py
@@ -3,9 +3,12 @@ import numpy as np
 
 def get_show() -> dict:
     df = pd.read_csv("S:\\Code\\genga\\genga-app\\data\\anime3.csv")
-    random_index = np.random.randint(1, 500)  # Random number between 1 and 200
-    selection = df.iloc[random_index]  # Select the row at the random index
-    print(selection)   # Print the selected row
-    return selection.to_dict()  # Convert the row to a dictionary
+    while True:
+        random_index = np.random.randint(1, len(df))  # Random number between 1 and the number of rows in the dataframe
+        selection = df.iloc[random_index]  # Select the row at the random index
+        if selection["English name"] != "Unknown":  # Skip if the English name is "Unknown"
+            print(selection)  # Print the selected row
+            return selection.to_dict()  # Convert the row to a dictionary
+
 
 # get_show()
